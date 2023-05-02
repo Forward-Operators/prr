6708f2f68214a373f6cb1eb317fdf9312ed16e17
Zbigniew Sobiecki <zbigniew@sobiecki.name>
2023-05-02 19:49:20 +0000

    basic command line

--- utils/run.py
+++ utils/run.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3.10
 
-import sys
+DEFAULT_MODEL = "openai/gpt-3.5-turbo"
 
+import sys
 sys.path.append('.')
 sys.path.append('/opt/conda/lib/python3.10/site-packages')
+
+import argparse
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -11,10 +14,19 @@ load_dotenv()
 from lib.prompt.library import Library
 from lib.runner import Runner
 
-library = Library("prompts")
-prompt = library.get("concept-maps/subconcepts-of-buddhism.txt")
+parser = argparse.ArgumentParser(description="Run a prompt against a model.",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-model = "openai/gpt-3.5-turbo"
+parser.add_argument("-m", "--model", help="model to use", default=DEFAULT_MODEL)
+# parser.add_argument("-v", "--verbose", action="store_true", help="increase verbosity")
+parser.add_argument("prompt_path", help="Path to prompt to run")
+args = parser.parse_args()
+config = vars(args)
+
+library = Library("prompts")
+prompt = library.get(config["prompt_path"])
+
+model = config["model"] or DEFAULT_MODEL
 # model = "anthropic/claude-v1"
 
 runner = Runner(prompt, model)
