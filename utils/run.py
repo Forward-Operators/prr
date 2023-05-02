53445be68f9308129e37bb7f753252060babd1c8
Zbigniew Sobiecki <zbigniew@sobiecki.name>
2023-05-02 07:46:52 +0000

    wip

--- utils/run.py
+++ utils/run.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3.10
 
 import sys
+
 sys.path.append('.')
-# sys.path.append('/home/codespace/.local/lib/python3.10/site-packages')
 sys.path.append('/opt/conda/lib/python3.10/site-packages')
 
 from dotenv import load_dotenv
 load_dotenv()
-
-from colored import fg, bg, attr
 
 from lib.prompt.library import Library
 from lib.runner import Runner
@@ -16,17 +14,11 @@ from lib.runner import Runner
 library = Library("prompts")
 prompt = library.get("concept-maps/subconcepts-of-buddhism.txt")
 
-runner = Runner(prompt, "openai/gpt-3.5-turbo")
-# runner = Runner(prompt, "anthropic/claude-v1")
+model = "openai/gpt-3.5-turbo"
+# model = "anthropic/claude-v1"
 
+runner = Runner(prompt, model)
 runner.run()
 
 result = runner.get_result()
 stats = runner.get_stats()
-
-# print ("=====================")
-
-# print (fg("green"))
-# print (result)
-# print (fg("red"))
-# print (stats)
