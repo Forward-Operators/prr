84e82a132851f635f5269f6b2d5d49fb1f3bb711
Zbigniew Sobiecki <zbigniew@sobiecki.name>
2023-06-06 11:51:20 +0000

    introducing monaco editor

---
+++ prr/ui/static/monaco-editor/min/vs/base/common/worker/simpleWorker.nls.it.js
@@ -0,0 +1,8 @@
+/*!-----------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.39.0-dev.20230605(8bb1a4e8636d1dd88e3cfa4a3556b315011dac57)
+ * Released under the MIT license
+ * https://github.com/microsoft/vscode/blob/main/LICENSE.txt
+ *-----------------------------------------------------------*/define("vs/base/common/worker/simpleWorker.nls.it",{"vs/base/common/platform":["_"]});
+
+//# sourceMappingURL=../../../../../min-maps/vs/base/common/worker/simpleWorker.nls.it.js.map
